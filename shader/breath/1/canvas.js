54705a14ad1dbf6925bd99e9bda74737d59958e7
Arnab Goswami <arnabiscoding@gmail.com>
2024-03-04 12:48:54 +0530

    texture changed

--- shader/breath/1/canvas.js
+++ shader/breath/1/canvas.js
@@ -1,4 +1,4 @@
 const canvas = document.querySelector("canvas");
 const sandbox = new GlslCanvas(canvas);
 sandbox.load(frag);
-sandbox.setUniform("displacement", "displacement1.jpg");
+sandbox.setUniform("displacement", "i.jpg");
