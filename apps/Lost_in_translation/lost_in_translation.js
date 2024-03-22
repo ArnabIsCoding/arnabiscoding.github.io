f57c1f707b181b86bee9de33d2cc7e5bb12c7af8
Arnab Goswami <arnabiscoding@gmail.com>
2024-03-22 16:57:17 +0530

    somethings

---
+++ apps/Lost_in_translation/lost_in_translation.js
@@ -0,0 +1,4 @@
+function loadGoogleTranslate() {
+	new load.translate.TranslateElement(document.getElementById("google_translate_element"), "en", "zh-CN");
+	// new google.translate.TranslateElement("google_element");
+}
