1c7a911b0522e194271fd4aed4fc346d7f9286c8
Arnab Goswami <arnabiscoding@gmail.com>
2024-03-26 00:00:02 +0530

    25march

--- DB/learn.js
+++ DB/learn.js
@@ -27,3 +27,26 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+const headingElements = document.querySelectorAll('h1[id], h2[id], h3[id]');
+  const links = document.querySelectorAll('a[href^="#"]');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const targetElement = entry.target;
+      const targetId = targetElement.id;
+      const correspondingLink = document.querySelector(`a[href="#${targetId}"]`);
+
+      if (entry.isIntersecting) {
+        correspondingLink.classList.add('viewrn');
+      } else {
+        correspondingLink.classList.remove('viewrn');
+      }
+    });
+  }, {
+    rootMargin: '0px 0px -50% 0px',
+  });
+
+  headingElements.forEach((headingElement) => {
+    observer.observe(headingElement);
+  });
