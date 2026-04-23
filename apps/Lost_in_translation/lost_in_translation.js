19145133abd429cefd49a88b991250691ed70376
Arnab Goswami <arnabiscoding@gmail.com>
2026-04-23 20:38:18 +0530

    Delete apps/Lost_in_translation directory

--- apps/Lost_in_translation/lost_in_translation.js
+++
@@ -1,4 +0,0 @@
-function loadGoogleTranslate() {
-	new load.translate.TranslateElement(document.getElementById("google_translate_element"), "en", "zh-CN");
-	// new google.translate.TranslateElement("google_element");
-}
